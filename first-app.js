e6329d269b39cd4293aefbead39ce8c26d1db055
um-mobile <trinlqFX11829@funix.edu.vn>
2021-12-04 17:17:41 +0700

    LAB1.1

--- first-app.js
+++ first-app.js
@@ -1,3 +1,5 @@
 var fs = require('fs');
 
-fs.writeFileSync('hello.txt', 'Hello from Node.js');
+fs.writeFileSync('hello.txt', 'Hello from Node.js'); 
+
+// writeFileSync tạo ra một file và giá trị.
