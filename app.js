035f2f6221f2d27aa5ef3977ee7999c9a7756886
um-mobile <trinlqFX11829@funix.edu.vn>
2021-12-04 17:13:21 +0700

    LAB1.3

--- app.js
+++ app.js
@@ -6,8 +6,15 @@ const myHttp = require('http'); // imort a file, hoặc lấy path đến tâ
 //  hoặc là một function mũi tên để gọn
 // }
 
-const server = myHttp.createServer((req,res) => {
-    console.log(req);
+const server = myHttp.createServer((req, res) => {
+    console.log(req.url, req.method, req.headers); // show request
+    //  process.exit(); thoát khỏi quá trình loop, huỷ đăng ký Listener.
+    res.setHeader('Content-Type', 'text/html'); // set giá trị header 
+    res.write('<html>');
+    res.write('<head><title>My First Page</title></head>');
+    res.write('<body><h1>Hello from my Node.js</h1></body>');
+    res.write('</html>');
+    res.end(); // Sau khi gọi end,không được viết nữa
 });
 
 server.listen(3000);
