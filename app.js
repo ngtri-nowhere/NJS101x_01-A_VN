285e905a4ade1d3a270851d300b005ff3c712c09
um-mobile <trinlqFX11829@funix.edu.vn>
2021-12-08 15:36:50 +0700

    LAB2.2

--- app.js
+++ app.js
@@ -9,9 +9,18 @@ const myHttp = require('http'); // imort a file, hoặc lấy path đến tâ
 // }
 // vì ta nhập vào không phải là một module toàn cục.
 // nên cần chỉ rõ đường dẫn. đây là một lõi file tuỳ chĩnh
-const express = require('express');
 
+const express = require('express');
 const app = express();
+// thuộc tính use của express cho ta thêm một hàm trung gian mới
+app.use((req, res, next) => {
+    console.log("In the middleware");
+    next();//next cho phép request được tiếp tục đến middleware tiếp theo
+});
+app.use((req, res, next) => {
+    console.log("In another middleware");
+});
+
 
 const server = myHttp.createServer(app);
 
