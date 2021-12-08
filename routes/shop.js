963b2b4db41fd7aee62d63a69088d41ce438f899
um-mobile <trinlqFX11829@funix.edu.vn>
2021-12-08 18:42:07 +0700

    LAB2.11

--- routes/shop.js
+++ routes/shop.js
@@ -1,3 +1,5 @@
+const path = require('path'); // sử dụng module này 
+
 const express = require('express')
 
 const router = express.Router();
@@ -5,7 +7,10 @@ const router = express.Router();
 router.get('/', (req, res, next) => {
     console.log("In Route shop middleware");
     // mặc định setHeader của express là text/html
-    res.send('<h1>Hello From Express</h1>');
+    res.sendFile(path.join(__dirname, '../', 'views', 'shop.html')); //join đường dẫn. 
+    //với mãng đầu __dirname là chỉ đường dẫn thư mục project
+    // mãng sau là thu mục mình muốn sử dung , và mãng sau là file của chúng ta sử dung.
+    //  res.sendFile('./views/shop.html');// nó sẽ không hiêu vì đây là đường dẫn hệ thống.
 });
 
 module.exports = router;
