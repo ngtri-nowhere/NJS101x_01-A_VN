24f0efa99977e18209c1b643043e6795c687c11f
um-mobile <trinlqFX11829@funix.edu.vn>
2021-12-08 18:00:14 +0700

    LAB2.9

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({ extended: false })) // nên tắt mặc định
 
-app.use(adminRoutes);
+app.use('/admin',adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
--- routes/admin.js
+++ routes/admin.js
@@ -1,13 +1,13 @@
 const express = require('express')
 
 const router = express.Router(); // tham số Router .
-
-router.use('/add-product', (req, res, next) => {
+// /admin/add-product => GET
+router.get('/add-product', (req, res, next) => {
     // mặc định setHeader của express là text/html
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
+    res.send('<form action="/admin/add-product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
 });
-
-router.post('/product', (req, res, next) => { // .get giống như use 
+// /admin/add-product => POST
+router.post('/add-product', (req, res, next) => { // .get giống như use 
     //có thể xài một path khoặc không nhưng nó chỉ kích hoạt 
     // những request sắp tới. 
     //post cũng tương tự nhưng sẽ làm việc với request post
