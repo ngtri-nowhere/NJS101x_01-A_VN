d57686e683c3752f92ad9c930b4c2df1ca6854fa
um-mobile <trinlqFX11829@funix.edu.vn>
2021-12-10 22:45:55 +0700

    LAB3.4

--- routes/admin.js
+++ routes/admin.js
@@ -11,7 +11,7 @@ const products = [];
 // /admin/add-product => GET
 router.get('/add-product', (req, res, next) => {
     // mặc định setHeader của express là text/html
-    res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
+    res.render('add-product', { pageTitle: 'Add Product' });
 });
 // /admin/add-product => POST
 router.post('/add-product', (req, res, next) => { // .get giống như use 
