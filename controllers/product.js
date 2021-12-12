1b369798d8f653ad19f64142aa941a72d79a385d
um-mobile <trinlqFX11829@funix.edu.vn>
2021-12-12 21:13:12 +0700

    LAB4.3

--- controllers/product.js
+++ controllers/product.js
@@ -1,4 +1,4 @@
-const products = [];
+const Product = require('../models/product');
 
 exports.getAddProduct = (req, res, next) => {
     // mặc định setHeader của express là text/html
@@ -16,14 +16,14 @@ exports.postAddProduct = (req, res, next) => { // .get giống như use
     // những request sắp tới. 
     //post cũng tương tự nhưng sẽ làm việc với request post
     //sử dụng get,post,delete,patch,put để lọc chúng,
-    products.push({ title: req.body.title });
+    const product = new Product(req.body.title);
+    product.save();
     console.log(req.body);
     res.redirect('/'); // thuộc tính redirec điều hướng route
 }
 
 exports.getProducts = (req, res, next) => {
-
- 
+    const products = Product.fetchAll();;
     res.render('shop', {
         prods: products,
         pageTitle: 'Shop',
