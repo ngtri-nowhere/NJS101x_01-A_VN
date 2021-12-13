fa08c6a0fbbc2c0b8ee6e34f58edf3191c4a6760
um-mobile <trinlqFX11829@funix.edu.vn>
2021-12-13 19:25:09 +0700

    LAB4.8

--- controllers/shop.js
+++ controllers/shop.js
@@ -10,6 +10,12 @@ exports.getProducts = (req, res, next) => {
   });
 };
 
+exports.getProduct = (req, res, next) => {
+    const proId = req.params.productId; //params chứ một tham số route
+    console.log(proId);
+    res.redirect('/');
+}
+
 exports.getIndex = (req, res, next) => {
   Product.fetchAll(products => {
     res.render('shop/index', {
