eb283acf0351c7cc74b166927fc62b8e07a70f0b
um-mobile <trinlqFX11829@funix.edu.vn>
2021-12-14 16:36:47 +0700

    LAB4.14

--- controllers/admin.js
+++ controllers/admin.js
@@ -4,6 +4,7 @@ exports.getAddProduct = (req, res, next) => {
   res.render('admin/edit-product', {
     pageTitle: 'Add Product',
     path: '/admin/add-product',
+    editing: false
   });
 };
 
@@ -22,11 +23,19 @@ exports.getEditProduct = (req, res, next) => {
   if (!editMode) {
     return res.redirect('/');
   }
-  res.render('admin/edit-product', {
-    pageTitle: 'Edit Product',
-    path: '/admin/edit-product',
-    editing: editMode // không thể để true , thây vào đó để giá trị
-  });
+  const prodId = req.params.productId;
+  Product.findById(prodId, product => {
+    if(!product) {
+      return req.redirect('/');
+    }
+    res.render('admin/edit-product', {
+      pageTitle: 'Edit Product',
+      path: '/admin/edit-product',
+      editing: editMode, // không thể để true , thây vào đó để giá trị
+      product:product
+    });
+  })
+  
 };
 
 
