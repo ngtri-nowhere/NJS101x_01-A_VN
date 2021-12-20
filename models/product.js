8fd661924e98354bd7d2254ab0a671c74a3bb9fa
um-mobile <trinlqFX11829@funix.edu.vn>
2021-12-20 18:48:05 +0700

    LAB5.7

--- models/product.js
+++ models/product.js
@@ -2,7 +2,6 @@ const db = require('../util/database');
 
 const Cart = require('./cart');
 
-
 module.exports = class Product {
   constructor(id, title, imageUrl, description, price) {
     this.id = id;
@@ -13,20 +12,19 @@ module.exports = class Product {
   }
 
   save() {
-    return db.execute('INSERT INTO products(title,price,imageUrl,description) VALUES (?,?,?,?)',
-    [
-      this.title,this.price,this.imageUrl,this.description
-    ])
+    return db.execute(
+      'INSERT INTO products (title, price, imageUrl, description) VALUES (?, ?, ?, ?)',
+      [this.title, this.price, this.imageUrl, this.description]
+    );
   }
 
-  static deleteById(id) {
-
-  }
+  static deleteById(id) { }
 
   static fetchAll() {
     return db.execute('SELECT * FROM products');
   }
 
-  static findById(id,) {
+  static findById(id) {
+    return db.execute('SELECT * FROM products WHERE products.id = ?', [id]);
   }
-}
+};
