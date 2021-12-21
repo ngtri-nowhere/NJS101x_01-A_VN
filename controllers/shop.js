db2a199dd045aebc5b46d3ab8eb8db3df6a8b726
um-mobile <trinlqFX11829@funix.edu.vn>
2021-12-21 19:17:21 +0700

    LAB5.12

--- controllers/shop.js
+++ controllers/shop.js
@@ -2,15 +2,13 @@ const Product = require('../models/product');
 const Cart = require('../models/cart');
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll()
-    .then(([rows, fieldData]) => {
-      res.render('shop/product-list', {
-        prods: rows,
-        pageTitle: 'Shop',
-        path: '/products'
-      });
-    })
-    .catch(err => console.log(err));;
+  Product.findAll().then(products => {
+    res.render('shop/product-list', {
+      prods: products,
+      pageTitle: 'Shop',
+      path: '/products'
+    });
+  }).catch(err => { console.log(err) });
 };
 
 exports.getProduct = (req, res, next) => {
@@ -22,19 +20,17 @@ exports.getProduct = (req, res, next) => {
         pageTitle: product.title,
         path: '/products'
       });
-    }).catch(err => console.log(err));
+    }).catch(err => { console.log(err) });
 };
 
 exports.getIndex = (req, res, next) => {
-  Product.fetchAll()
-    .then(([rows, fieldData]) => {
-      res.render('shop/index', {
-        prods: rows,
-        pageTitle: 'Shop',
-        path: '/'
-      });
-    })
-    .catch(err => console.log(err));
+  Product.findAll().then(products => {
+    res.render('shop/index', {
+      prods: products,
+      pageTitle: 'Shop',
+      path: '/'
+    });
+  }).catch(err => { console.log(err) });
 
 };
 
