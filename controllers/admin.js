c3a770681a8ee9bf9f0238d44d16484536c9c9c9
um-mobile <trinlqFX11829@funix.edu.vn>
2021-12-21 18:28:10 +0700

    'LAB5.11'

--- controllers/admin.js
+++ controllers/admin.js
@@ -13,10 +13,17 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  const product = new Product(null, title, imageUrl, description, price);
-  product.save().then(() => {
-    res.redirect('/');
-  }).catch(err => console.log(err));
+  Product.create({
+    title: title,
+    price: price,
+    imageUrl: imageUrl,
+    description: description
+  }).then(result => {
+    // console.log(result);
+    console.log('Created a product');
+  }).catch(err => {
+    console.log(err);
+  });
 };
 
 exports.getEditProduct = (req, res, next) => {
