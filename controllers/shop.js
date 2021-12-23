da959ab4aad40961f11f92fa0c1aed98aa7e74ac
um-mobile <trinlqFX11829@funix.edu.vn>
2021-12-24 00:33:24 +0700

    LAB5.27

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,6 +1,7 @@
 const Product = require('../models/product');
 const Cart = require('../models/cart');
 
+
 exports.getProducts = (req, res, next) => {
   Product.findAll()
     .then(products => {
@@ -154,6 +155,34 @@ exports.postCartDeleteProduct = (req, res, next) => {
       console.log(err)
     })
 };
+
+exports.postOrder = (req, res, next) => {
+  req.user.getCart()
+    .then(cart => {
+      return cart.getProducts();
+    })
+    .then(products => {
+      return req.user
+        .createOrder()
+        .then(order => {
+          return order.addProducts(products.map(product => {
+            product.orderItem = { quantity: product.cartItem.quantity };
+            return product;
+          })
+          );
+        })
+        .then(result => {
+          res.redirect('/orders');
+        })
+        .catch(err => {
+          console.log(err)
+        });
+
+    })
+    .catch(err => {
+      console.log(err)
+    })
+}
 
 exports.getOrders = (req, res, next) => {
   res.render('shop/orders', {
