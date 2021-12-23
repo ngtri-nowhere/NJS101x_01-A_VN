2d4041d231faea2392efbd2ed7341d6ff01780a3
um-mobile <trinlqFX11829@funix.edu.vn>
2021-12-23 18:46:18 +0700

    LAB5.18

--- app.js
+++ app.js
@@ -13,9 +13,19 @@ app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const User = require('./models/user');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use((req, res, next) => {
+  User.findByPk(1)
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => console.error(err));
+})
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
@@ -23,11 +33,25 @@ app.use(shopRoutes);
 app.use(errorController.get404);
 
 sequelize
+  // .sync({force: true})
   .sync()
   .then(result => {
+    return User.findByPk(1)
     // console.log(result);
+  }).then(user => {
+    if (!user) {
+      return User.create({
+        name: 'Max',
+        email: 'max@example.com',
+      });
+    }
+    return Promise.resolve(user)
+  })
+  .then(user => {
+    console.log(user);
     app.listen(3000);
   })
   .catch(err => {
     console.log(err);
   });
+
