70c3ee391c270f3b2caebcfb03785584fbc9099b
um-mobile <trinlqFX11829@funix.edu.vn>
2021-12-23 20:06:05 +0700

    LAB5.20

--- controllers/admin.js
+++ controllers/admin.js
@@ -35,8 +35,10 @@ exports.getEditProduct = (req, res, next) => {
     return res.redirect('/');
   }
   const prodId = req.params.productId;
-  Product.findById(prodId)
-    .then(product => {
+  req.user.getProducts({ where: { id: prodId } })
+    // Product.findByPk(prodId)
+    .then(products => {
+      const product = products[0]
       if (!product) {
         return res.redirect('/');
       }
@@ -56,7 +58,7 @@ exports.postEditProduct = (req, res, next) => {
   const updatedPrice = req.body.price;
   const updatedImageUrl = req.body.imageUrl;
   const updatedDesc = req.body.description;
-  Product.findById(prodId)
+  Product.findByPk(prodId)
     .then(product => {
       product.title = updatedTitle;
       product.price = updatedPrice;
@@ -72,7 +74,7 @@ exports.postEditProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-  Product.findAll()
+ req.user.getProducts()
     .then(products => {
       res.render('admin/products', {
         prods: products,
@@ -85,7 +87,7 @@ exports.getProducts = (req, res, next) => {
 
 exports.postDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.findById(prodId)
+  Product.findByPk(prodId)
     .then(product => {
       return product.destroy();
     })
