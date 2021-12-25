48e8af097ac4212ab0d1a4e3b2f5f9e9b7250c07
um-mobile <trinlqFX11829@funix.edu.vn>
2021-12-25 18:02:55 +0700

    LAB6.1

--- util/database.js
+++ util/database.js
@@ -1,8 +1,15 @@
-const Sequelize = require('sequelize');
+const mongodb = require('mongodb');
 
-const sequelize = new Sequelize('node-complete', 'root', '123456789', {
-  dialect: 'mysql',
-  host: 'localhost'
-});
+const MongoClient = mongodb.MongoClient
 
-module.exports = sequelize;
+const mongoConnect = (callback) => {
+  MongoClient.connect('mongodb+srv://user_um25:4wppAUGuieaom0Qb@cluster0.mikrv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
+    .then(client => {
+      console.log('Connected');
+      callback(client)
+    })
+    .catch(err => { console.log(err) });
+
+}
+
+module.exports = mongoConnect;
