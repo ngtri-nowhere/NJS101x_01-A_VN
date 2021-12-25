d871666d8855b5c038adcbfa9dfabd0d20764a68
um-mobile <trinlqFX11829@funix.edu.vn>
2021-12-25 18:37:07 +0700

    LAB6.3

--- util/database.js
+++ util/database.js
@@ -1,15 +1,28 @@
 const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
 
-const MongoClient = mongodb.MongoClient
+let _db;
 
 const mongoConnect = (callback) => {
-  MongoClient.connect('mongodb+srv://user_um25:4wppAUGuieaom0Qb@cluster0.mikrv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
+  MongoClient.connect('mongodb+srv://user_um25:4wppAUGuieaom0Qb@cluster0.mikrv.mongodb.net/shop?retryWrites=true&w=majority')
     .then(client => {
       console.log('Connected');
+      _db = client.db() // luu mot ket noi voi database
       callback(client)
     })
-    .catch(err => { console.log(err) });
+    .catch(err => {
+      console.log(err);
+      throw err;
 
+    });
 }
 
-module.exports = mongoConnect;
+const getDb = () => {
+  if (_db) {
+    return _db
+  }
+  throw 'No Database found!';
+}
+
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
