222679e1b3126b6e86715059427e54a408e32362
um-mobile <trinlqFX11829@funix.edu.vn>
2021-12-27 00:44:57 +0700

    LAB6.16

--- models/user.js
+++ models/user.js
@@ -4,15 +4,32 @@ const getDb = require('../util/database').getDb;
 const ObjectId = mongodb.ObjectId
 
 class User {
-  contructor(username, email) {
+  contructor(username, email, cart, id) {
     this.name = username;
     this.email = email;
+    this.cart = cart;
+    this._id = id;
   }
   save() {
     const db = getDb();
     return db.collection('users').insertOne(this)
       .then()
       .catch(err => { console.log(err) })
+  }
+
+  addToCart(product) {
+    // const cartProduct = this.cart.items.findIndex(cp => {
+    //   return cp.id === product.id;
+    // })
+
+
+    const updatedCart = { items: [{ ...product, quantity: 1 }] };
+    const db = getDb();
+    return db.collection('users')
+      .updateOne({ _id: new ObjectId(this._id) }, {
+        $set: { cart: updatedCart }
+      });
+
   }
 
   static findById(userId) {
