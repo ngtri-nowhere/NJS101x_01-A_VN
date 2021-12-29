1c1b48dd3cda435b49861cee2377cced40216c8c
um-mobile <trinlqFX11829@funix.edu.vn>
2021-12-29 21:43:50 +0700

    LAB6.17.fix

--- models/user.js
+++ models/user.js
@@ -35,7 +35,7 @@ class User {
     // const updatedCart = {
     //   items: updatedCartItems
     // };
-    const updatedCart = { items: [{ ...product, quantity: 1 }] };
+    const updatedCart = { items: [{ productId: new ObjectId(product._id), quantity: 1 }] };
     const db = getDb();
     return db
       .collection('users')
