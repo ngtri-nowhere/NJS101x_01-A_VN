877e537d97b1f635029c5264f5a32f0193976187
um-mobile <trinlqFX11829@funix.edu.vn>
2022-01-04 06:16:37 +0700

    LAB7.08

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
 const errorController = require('./controllers/error');
-// const User = require('./models/user');
+const User = require('./models/user');
 
 const app = express();
 
@@ -19,13 +19,12 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
-  // User.findById('61cc659bdb910e54e904d285')
-  //   .then(user => {
-  //     req.user = new User(user.name, user.email, user.cart, user._id);
-  //     next();
-  //   })
-  //   .catch(err => console.log(err));
-  next();
+  User.findById('61d383945a31ecd1d3bddddb')
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => console.log(err));
 });
 
 app.use('/admin', adminRoutes);
@@ -35,6 +34,18 @@ app.use(errorController.get404);
 
 mongoose.connect('mongodb+srv://user_um25:4wppAUGuieaom0Qb@cluster0.mikrv.mongodb.net/shop?retryWrites=true&w=majority')
   .then(result => {
+    User.findOne().then(user => {
+      if (!user) {
+        const user = new User({
+          name: "Max",
+          email: "Max@gmail.com",
+          cart: {
+            items: []
+          },
+        });
+        user.save();
+      }
+    });
     app.listen(3000);
     console.log("it's work")
   })
