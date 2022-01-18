111471b8eda21cb8790c986d2562da450658f458
um-mobile <trinlqFX11829@funix.edu.vn>
2022-01-18 11:18:42 +0700

    LAB9.4

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { check } = require('express-validator/check');
+const { check, body } = require('express-validator/check');
 
 const authController = require('../controllers/auth');
 
@@ -11,15 +11,21 @@ router.get('/signup', authController.getSignup);
 
 router.post('/login', authController.postLogin);
 
-router.post('/signup', check('email')
-    .isEmail()
-    .withMessage('Please enter a valid email.')
-    .custom((value, { req }) => {
-        if (value === 'test@test.com') {
-            throw new Error('This email address if forbidden.')
-        }
-        return true
-    }), authController.postSignup);
+router.post('/signup', [
+    check('email')
+        .isEmail()
+        .withMessage('Please enter a valid email.')
+        .custom((value, { req }) => {
+            if (value === 'test@test.com') {
+                throw new Error('This email address if forbidden.')
+            }
+            return true
+        }),
+    body('password', 'Please enter a password with only numbers and text and at least 5 characters')
+        .isLength({ min: 5 })
+        .isAlphanumeric()
+],
+    authController.postSignup);
 
 router.post('/logout', authController.postLogout);
 
