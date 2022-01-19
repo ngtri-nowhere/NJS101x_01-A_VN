35ffbed8a4b001e16cffd587646777949f225a02
um-mobile <trinlqFX11829@funix.edu.vn>
2022-01-19 13:43:57 +0700

    LAB10.3

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const session = require('express-session');
 const MongoDBStore = require('connect-mongodb-session')(session);
 const csrf = require('csurf');
 const flash = require('connect-flash');
+const multer = require('multer');
 
 const errorController = require('./controllers/error');
 const User = require('./models/user');
@@ -29,6 +30,9 @@ const shopRoutes = require('./routes/shop');
 const authRoutes = require('./routes/auth');
 
 app.use(bodyParser.urlencoded({ extended: false })); // đây là phần mềm trung gian, với urlencoded sẽ phân tích dữ liệu về văn bản. url, string
+app.use(multer({
+  dest: 'images'
+}).single('image'))
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(
   session({
